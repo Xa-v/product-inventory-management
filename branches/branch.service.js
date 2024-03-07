eb455a5826ea249e2f0fb8f57ec57ff23c509e85
Xav <xav1234g@gmail.com>
2024-03-07 16:36:44 +0800

    2nd commit

---
+++ branches/branch.service.js
@@ -0,0 +1,106 @@
+const bcrypt = require("bcryptjs");
+const db = require("_helpers/db");
+
+module.exports = {
+  getAll,
+  getBranch,
+  create,
+  update,
+  delete: _delete,
+  assignUserToBranch,
+};
+
+async function getAll() {
+    return await db.Branches.findAll();
+  }
+
+  async function getBranch(id) {
+    const branch = await db.Branches.findByPk(id);
+    if (!branch) throw "branch not found";
+    return branch;
+  }
+
+
+  async function create(params) {
+    const branch = new db.Branches(params);
+    await branch.save();
+  }
+  
+
+  async function update(id, params) {
+    const branch = await getBranch(id);
+    console.log("Initial branch status:", branch.status);
+  
+   
+    const branchChanged = branch.branch !== params.branch;
+    const statusChanged = branch.status !== params.status;
+  
+    if (
+      branchChanged &&
+      (await db.Branches.findOne({ where: { branch: params.branch } }))
+    ) {
+      console.log('Branch "' + params.branch + '" is already registered');
+    }
+  
+    if (statusChanged) {
+      console.log("Before setting status:", branch.status);
+  
+      branch.status = params.status;
+      console.log("After setting status:", branch.status);
+    } else {
+      console.log('Status "' + branch.status + '" is already registered');
+    }
+  
+    
+  
+    Object.assign(branch, params);
+    await branch.save();
+  }
+  
+
+  async function _delete(id) {
+    const branch = await getBranch(id);
+    await branch.destroy();
+  }
+  
+
+
+  async function assignUserToBranch(branchId, userId) {
+    try {
+      const existingAssignment = await db.UserBranch.findOne({
+        where: { userId, branchId },
+      });
+  
+      if (existingAssignment) {
+        throw { status: 400, message: "User already assigned to this branch" };
+      }
+      // Check if the branch and user exist in the database
+      const branch = await db.Branches.findByPk(branchId, {
+        attributes: ["id", "name", "location", "status"],
+      });
+      const user = await db.User.findByPk(userId, {
+        attributes: ["id", "email", "passwordHash", "username"],
+      });
+  
+      if (!branch || !user) {
+        throw { status: 404, message: "Branch or user not found" };
+      }
+  
+      // Assign the user to the branch (update the database accordingly)
+      user.assignedBranchId = branchId;
+      await user.save();
+  
+      await db.UserBranch.create({
+        userId: user.id,
+        username: user.username,
+        branchId: branch.id,
+        branchName: branch.name,
+      });
+  
+      // Respond with a success message
+      return "User assigned to branch successfully";
+    } catch (error) {
+      console.error(error);
+      throw { status: 500, message: "Internal Server Error" };
+    }
+  }
---
+++ inventory/inventory.service.js
@@ -0,0 +1,81 @@
+const db = require("_helpers/db");
+const {Op} = require("sequelize");
+const Role = require("_helpers/role");
+
+module.exports = {
+  getAll,
+  createNewInventory,
+  viewInventory,
+};
+
+async function getAll() {
+  return await db.Inventory.findAll();
+}
+
+async function createNewInventory(params, role) {
+  authorize(role, [Role.Admin, Role.Manager]);
+
+  const {productID, stockIn, stockOut} = params;
+
+  // Find the product by ID
+  const product = await db.Product.findByPk(productID);
+
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+ 
+
+  if (stockIn && stockOut) {
+    throw new Error("Select only one Stock attribute to update");
+  }
+
+  // Update product stock based on the operation
+  if (stockIn) {
+    product.productStocks += stockIn;
+  } else if (stockOut) {
+    if (product.productStocks < stockOut) {
+      throw new Error("Insufficient stock");
+    }
+    product.productStocks -= stockOut;
+  } else {
+    throw new Error("Error: Please add value to the stocks");
+  }
+
+  // Save the updated product
+  await product.save();
+
+  // Create a new inventory record with the product name
+  await db.Inventory.create({
+    productID: product.id,
+    productName: product.productname,
+    stockIn: stockIn || 0,
+    stockOut: stockOut || 0,
+  });
+}
+
+async function viewInventory({role}) {
+  authorize(role, [Role.Admin, Role.Manager]);
+
+  const products = await db.Product.findAll({
+    where: {
+      isActive: true,
+    },
+    attributes: ["id", "productName", "productStocks"],
+  });
+
+  if (products.length === 0) {
+    throw "The Inventory is empty.";
+  }
+
+  return products;
+}
+
+function authorize(role, allowedRoles) {
+  if (
+    !role ||
+    !allowedRoles.map((r) => r.toLowerCase()).includes(role.toLowerCase())
+  ) {
+    throw "Unauthorized User";
+  }
+}
